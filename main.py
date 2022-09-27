0fb5503289d342c0eaad741e988f4cdf4806f869
root <root@DESKTOP-M50ELVH.localdomain>
2022-09-28 02:43:53 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+from multiprocessing.dummy import Pool as ThreadPool
+
+def checkDebug(site):
+    try:
+        r = requests.get(site)
+        if 'PhpDebugBar' in r.text:
+            print('[{}] > Vulnerable!'.format(site))
+            saveres = open('result.txt', 'a')
+            saveres.write(site+'\n')
+        else:
+            pass
+    except:
+        pass
+
+print("Laravel Debugbar\nAuthor: angga1337\n")
+site = open(input('Sitelist: '),'r').read().splitlines()
+Thread = int(input('Thread: '))
+pool = ThreadPool(Thread)
+pool.map(checkDebug, site)
+pool.close()
+pool.join()
